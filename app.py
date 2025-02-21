70652e8dd6e08fa3c7cfe9bad9b743cb90bd062c
Ayushh26-Cacti <akulshreshtha985@gmail.com>
2025-02-22 01:16:05 +0530

    Outline

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ def index():
     render_template("index.html")
 
 
+# add enctype="multipart/form-data" on form attribute
+@app.route("/result", method=["GET", "POST"])
+def result():
+
+    # Getting file
+    f = request.files["video"]
+
+    # Giving request to model for data, and getting it as a dict
+    # TODO
+    data = {}
+    
+    # returning JSON output (variable - data)
+    dataJSON = jsonify(data)
+    render_template("result.html", data=data)
+
